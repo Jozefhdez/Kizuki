71b62a5f3f5c48666a5713c81d82630b2bc4fb9a
Jozef David Hernández Campos <jozefhdez2@gmail.com>
2025-07-08 04:09:48 -0600

    fix: font not applying to inputs and logout button on dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,19 @@
+import { useNavigate } from "react-router-dom";
+import { supabase } from "../services/supabaseClient";
+
 function Dashboard() {
+  const navigate = useNavigate();
+  
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    navigate("/");
+  };
+
   return (
+    <div className="dashboard-container">
     <div>Dashboard</div>
+        <button onClick={handleLogout}> Sign Out </button>
+    </div>
   )
 }
 
