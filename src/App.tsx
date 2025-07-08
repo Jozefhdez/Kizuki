81226c8067ee959cc3e95efdb92cfef2a73a757f
Jozef David Hernández Campos <jozefhdez2@gmail.com>
2025-07-08 01:46:03 -0600

    feat: add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import kizukiLogo from '/src/assets/kizuki.svg'
 import './App.css'
 
 function App() {
